c0a1d4839f7e0e3bc90e701f7befd5c2e2ec9a74
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-05-30 17:31:43 -0700

    Feature Complete
    Feature complete.

---
+++ TextField/Text Field Delegates/EmojiTextFieldDelegate.swift
@@ -0,0 +1,71 @@
+/*
+ *  EmojiTextFieldDelegate.swift
+ *  TextField
+ *
+ *  Created by Paul Lewallen on 5/30/23.
+ *
+ */
+
+import UIKit
+
+// MARK: - EmojiTextFieldDelegate : NSObject, UITextFieldDelegate
+class EmojiTextFieldDelegate : NSObject, UITextFieldDelegate {
+    
+    // MARK: Properties
+    var translations = [String : String]()
+    
+    // MARK: Initializer
+    override init() {
+        super.init()
+        
+        translations["heart"] = "\u{0001F496}"
+        translations["fish"] = "\u{E522}"
+        translations["bird"] = "\u{E523}"
+        translations["frog"] = "\u{E531}"
+        translations["bear"] = "\u{E527}"
+        translations["dog"] = "\u{E052}"
+        translations["cat"] = "\u{E04F}"
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var replacedAnEmoji = false
+        var emojiStringRange: NSRange
+        
+        // Construct the text that will be in the field if this change is accepted
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        // For each dictionary entry in translations, pull out a string to search for
+        // and an emoji to replace it with
+        
+        for (emojiString, emoji) in translations {
+            
+            // Search for all occurences of key (ie. "dog"), and replace with emoji (ie. 🐶)
+            repeat {
+                emojiStringRange = newText.range(of: emojiString, options: .caseInsensitive)
+                
+                // Found one
+                if emojiStringRange.location != NSNotFound {
+                    newText = newText.replacingCharacters(in: emojiStringRange, with: emoji) as NSString
+                    replacedAnEmoji = true
+                }
+            } while emojiStringRange.location != NSNotFound
+        }
+        
+        // If we have replaced an emoji, then we directly edit the text field
+        // Otherwise we allow the proposed edit.
+        if replacedAnEmoji {
+            textField.text = newText as String
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    
+}
--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -19,23 +19,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Text Field Delegate objects
     let emojiDelegate = EmojiTextFieldDelegate()
-    let colorizeDelegate = ColorizerTextFieldDelegate()
+    let colorizerDelegate = ColorizerTextFieldDelegate()
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the label to be hidden
+        self.characterCountLabel.isHidden = true
         
         // Set the three delegates
         self.textField1.delegate = emojiDelegate
-        self.textField2.delegate = colorizeDelegate
+        self.textField2.delegate = colorizerDelegate
         self.textField3.delegate = self
     }
     
     // MARK: Text Field Delegate Methods
-    func textField() {
-        //stub
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // Figure out what the new text will be, if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        // Hide the label if the newText will be an empty string
+        self.characterCountLabel.isHidden = (newText.length == 0)
+        
+        // Write the length of newText into the label
+        self.characterCountLabel.text = String(newText.length)
+        
+        // Returning true gives the text field permission to change its text
+        return true;
     }
+    
+        
 }
-
