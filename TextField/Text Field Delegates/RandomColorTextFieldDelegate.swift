991dea9a345c8cd9d9f41df4367f138c725edfc7
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-05-30 18:42:04 -0700

    Feature Complete
    Feature complete.

--- TextField/Text Field Delegates/RandomColorTextFieldDelegate.swift
+++ TextField/Text Field Delegates/RandomColorTextFieldDelegate.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class RandomColorTextFieldDelegate: NSObject, UITextFieldDelegate {
-    //func randomColor() ->UIColor {
-    //@TODO: return a random color
-    //}
+    
+    let colors:[UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .brown]
+    
+    func randomColor() ->UIColor {
+        let randomIndex = Int(arc4random() % UInt32(colors.count))
+        return colors[randomIndex]
+    }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        //@TODO: Set the color of your text here!
+        textField.textColor = randomColor()
         return true
     }
 }
